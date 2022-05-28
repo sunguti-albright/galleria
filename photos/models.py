cdedfcdcd5c8424218433208530cc176d0afaa12
albright.human <sungutialbright@gmail.com>
2022-05-28 17:18:30 +0300

    add image model

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image_name = models.CharField(max_length=200),
+    image_description = models.TextField(),
+    image = models.ImageField(upload_to= 'images/', default='')
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def update_image(self, Name=None, category=None):
+        self.image_name = Name if Name else self.Name
+        self.image_category = category if category else self.image_category 
+        self.save()
+
+
+    def __str__(self):
+        return self.image_name

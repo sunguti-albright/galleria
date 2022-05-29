e73645e14407b9becac43e9904e42feccca8f75a
albright.human <sungutialbright@gmail.com>
2022-05-29 11:08:27 +0300

    add functions to display images and search templates

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+import datetime as dt
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+def search(request):
+    return render(request, 'search.html')

4490d46639b2a1c66893bbf2f349110511ee14ab
albright.human <sungutialbright@gmail.com>
2022-05-29 11:07:11 +0300

    add gallery and search url

--- photos/urls.py
+++ photos/urls.py
@@ -1,10 +1,14 @@
 from django.urls import include,re_path as url
 from . import views
+from unicodedata import name
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
    url(r'^$', views.index, name='index'),
-    url(r'^gallery$', views.gallery, name='gallery'),
+   url(r'^gallery$', views.gallery, name='gallery'),
+   url(r'^search/', views.search_category, name = 'search_category')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

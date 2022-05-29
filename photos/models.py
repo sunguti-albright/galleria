cf7a8a19287802e2acb18e6ad37908f0436755fe
albright.human <sungutialbright@gmail.com>
2022-05-29 11:04:48 +0300

    add CharField for image description

--- photos/models.py
+++ photos/models.py
@@ -1,11 +1,13 @@
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
 class Image(models.Model):
-    image_name = models.CharField(max_length=200),
-    image_description = models.TextField(),
+    image_name = models.CharField(max_length=100),
+    image_description = models.CharField(max_length=200),
     image = models.ImageField(upload_to= 'images/', default='')
-
+    image_location = models.ForeignKey('Location',on_delete=models.CASCADE,default='')
+    image_category = models.ForeignKey('Category',on_delete=models.CASCADE,default='')
     def save_image(self):
         self.save()
 
@@ -20,3 +22,30 @@ class Image(models.Model):
 
     def __str__(self):
         return self.image_name
+
+   
+class Category(models.Model):
+    category_name = models.CharField(max_length =30)
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    def __str__(self):
+        return self.category_name
+
+
+class Location(models.Model):
+    location_name = models.CharField(max_length=30)
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    def __str__(self):
+        return self.location_name
+
